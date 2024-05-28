6ac5f371d30c5acfc8facbc964e61a78201a5709
Abror95 <abror.safarov.95@gmail.com>
2024-05-29 01:27:23 +0500

    done

--- src/Abror/Week1/Task1Division.java
+++ src/Abror/Week1/Task1Division.java
@@ -3,6 +3,17 @@ package Abror.Week1;
 public class Task1Division {
     public static void main(String[] args) {
         //Write a method that can divide two numbers without using division operator
-// change
+        int a = 20,
+        b= 5;
+        int d=0;
+
+        while ( a-b >= 0){
+
+            a = a - b;
+            ++d;
+        }
+
+        System.out.println(d);
+
     }
 }
