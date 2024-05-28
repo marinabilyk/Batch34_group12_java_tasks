a6de35ee16ddfb5b3e9700beb26344ee61e0745c
Abror95 <abror.safarov.95@gmail.com>
2024-05-28 21:51:00 +0500

    updated

--- src/Abror/Week1/Task1Division.java
+++ src/Abror/Week1/Task1Division.java
@@ -3,6 +3,6 @@ package Abror.Week1;
 public class Task1Division {
     public static void main(String[] args) {
         //Write a method that can divide two numbers without using division operator
-ahjashsha
+// change
     }
 }
